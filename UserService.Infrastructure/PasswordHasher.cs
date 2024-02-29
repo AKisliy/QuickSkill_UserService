0a116fd37f3e1d5bae2fd615e5354ee1c0b291af
AKisliy <lekha.a-kisel@yandex.ru>
2024-02-29 18:26:52 +0300

    Start doing auth logic

---
+++ UserService.Infrastructure/PasswordHasher.cs
@@ -0,0 +1,13 @@
+using UserService.Core.Interfaces.Auth;
+
+namespace UserService.Infrastructure
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Generate(string password)
+        {
+            return BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+        }
+
+    }
+}
--- UserService.Infrastructure/obj/Debug/net8.0/UserService.Infrastructure.AssemblyInfo.cs
+++ UserService.Infrastructure/obj/Debug/net8.0/UserService.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb0b3c13b84bc8fb3990b704b2658d1d1934daa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4206e4bd414231583e9b6a91c821ded1c0b4281a")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
